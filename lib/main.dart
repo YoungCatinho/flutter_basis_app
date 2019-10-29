e7fbf5ca6fbdba8d58d26fb0d027aafbfd56fb55
YoungCatinho <nk-97@t-online.de>
2019-10-29 14:52:13 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basis_app/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Neue Basis App',
+      theme: ThemeData(
+        // This is the theme of your application.
+
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
